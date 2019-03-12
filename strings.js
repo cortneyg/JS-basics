f9330bdb789dd9b5785d9f754fa243bc6e22f129
Cortney Gardner <gardner.cortney@yahoo.com>
2019-03-12 11:36:41 -0600

    created variables, strings and string concatenation

---
+++ strings.js
@@ -0,0 +1,13 @@
+// let firstName = 'Cortney';
+// let lastName = 'Gardner';
+// let fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+//Challenge
+
+let city = 'Herriman';
+let country = 'United States';
+let location = city + ', ' + country;
+
+console.log(location);
